85d3a5d194c2946a59b0a881928974788b9481ac
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-07-22 00:03:02 +0430

    added add-customer

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
   if (mongoose.connections[0].readyState) return;
-
+  mongoose.set("strictQuery", false);
   await mongoose.connect(process.env.MONGO_URL);
   console.log("Connected to DB");
 }

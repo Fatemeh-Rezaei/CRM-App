f399e40eb89334acc5399264c4fc3193a02e16c4
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-07-21 15:44:59 +0430

    created connection to db

---
+++ utils/connectDB.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+async function connectDB() {
+  if (mongoose.connections[0].readyState) return;
+
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("Connected to DB");
+}
+
+export default connectDB;

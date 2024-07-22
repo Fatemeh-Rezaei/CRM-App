854c6b1865c98ea6539b3fe92408e69b4a93011d
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-07-22 17:20:26 +0430

    completed form input

--- components/module/Form.js
+++ components/module/Form.js
@@ -1,8 +1,27 @@
+import { inputs } from "../../constant/inputs";
+import FormInput from "./FormInput";
 import ItemList from "./ItemList";
 
 function Form({ form, setForm }) {
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+    console.log(form);
+  };
+
   return (
     <div>
+      {inputs.map((input, index) => (
+        <FormInput
+          key={index}
+          name={input.name}
+          label={input.label}
+          type={input.type}
+          value={form[input.name]}
+          onChange={changeHandler}
+        />
+      ))}
+
       <ItemList form={form} setFrom={setForm} />
     </div>
   );
---
+++ constant/inputs.js
@@ -0,0 +1,9 @@
+export const inputs = [
+  { name: "name", label: "Name", type: "text" },
+  { name: "lastName", label: "Last Name", type: "text" },
+  { name: "email", label: "Email", type: "text" },
+  { name: "phone", label: "Phone", type: "tel" },
+  { name: "address", label: "Address", type: "text" },
+  { name: "postalCode", label: "Postal Code", type: "text" },
+  { name: "date", label: "Date", type: "date" },
+];

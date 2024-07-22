3b1d05c65b1c8e5572a0da9998d0706bdc17c784
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-07-22 18:07:35 +0430

    added home page

---
+++ components/module/Card.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+
+function Card(customer) {
+  const deleteHandler = () => {};
+
+  return (
+    <div className="card">
+      <div className="card__details">
+        <p>
+          {customer.name} {customer.lastName}
+        </p>
+        <p>{customer.email}</p>
+      </div>
+      <div className="card__buttons">
+        <button onClick={deleteHandler}>Delete</button>
+        <Link href={`/edit/${customer._id}`}>Edit</Link>
+        <Link href={`/details/${customer._id}`}>Details</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import connectDB from "../utils/connectDB";
+import Customer from "../models/Customer";
+import HomePage from "../components/template/HomePage";
 
-function Index() {
-  return (
-    <div>Index</div>
-  )
+function Index({ customers }) {
+  return <HomePage customers={customers} />;
 }
 
-export default Index
+export default Index;
+
+export async function getServerSideProps() {
+  try {
+    await connectDB();
+    const customer = await Customer.find();
+    return {
+      props: {
+        customers: JSON.parse(JSON.stringify(customer)),
+      },
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
+}
